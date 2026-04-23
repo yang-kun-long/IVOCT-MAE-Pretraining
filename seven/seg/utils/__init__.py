a6abcd1afedd822286797a059d1cb608b6b89e7b
yang-kun-long <78290277+yang-kun-long@users.noreply.github.com>
2026-04-23 15:54:16 +0800

    Fix utils __init__: export aggregate_metrics

--- seven/seg/utils/__init__.py
+++ seven/seg/utils/__init__.py
@@ -1,5 +1,5 @@
 from .seg_losses import dice_loss, seg_loss
-from .seg_metrics import compute_metrics
+from .seg_metrics import compute_metrics, aggregate_metrics
 from .seg_vis import save_seg_visualization
 
-__all__ = ["dice_loss", "seg_loss", "compute_metrics", "save_seg_visualization"]
+__all__ = ["dice_loss", "seg_loss", "compute_metrics", "aggregate_metrics", "save_seg_visualization"]
